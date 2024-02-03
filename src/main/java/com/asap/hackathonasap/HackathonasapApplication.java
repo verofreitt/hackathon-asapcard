7ed2bf74362e5c602ebefe6c8318bdde7b59c9b2
verofreitt <verofreit@gmail.com>
2024-02-03 10:57:01 -0300

    atualizacao properties

--- src/main/java/com/asap/hackathonasap/HackathonasapApplication.java
+++ src/main/java/com/asap/hackathonasap/HackathonasapApplication.java
@@ -1,13 +1,45 @@
 package com.asap.hackathonasap;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.asap.hackathonasap.Repository.InstallmentRepository;
+import com.asap.hackathonasap.Repository.PersonRepository;
+import com.asap.hackathonasap.Repository.TransactionRepository;
+
 @SpringBootApplication
-public class HackathonasapApplication {
+public class HackathonasapApplication implements CommandLineRunner {
+	
+	@Autowired
+	private PersonRepository personRepository;
+	
+	@Autowired 
+	private TransactionRepository transationRepository;
+	
+	@Autowired
+	private InstallmentRepository installRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HackathonasapApplication.class, args);
 	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void processInputData(String filePath) throws IOException {
+		
+		
+	}
+
+	
+	
 
 }
--- src/main/java/com/asap/hackathonasap/Repository/InstallmentRepository.java
+++ src/main/java/com/asap/hackathonasap/Repository/InstallmentRepository.java
@@ -2,6 +2,8 @@ package com.asap.hackathonasap.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.asap.hackathonasap.model.Installment;
+
 public interface InstallmentRepository extends JpaRepository<Installment, String>{
 
 }
