8cca61b5dea2f59bcc9c813c943d81e32aee329b
verofreitt <verofreit@gmail.com>
2024-02-03 11:38:42 -0300

    atualizacoes pom

--- src/main/java/com/asap/hackathonasap/HackathonasapApplication.java
+++ src/main/java/com/asap/hackathonasap/HackathonasapApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.asap.hackathonasap.Repository.InstallmentRepository;
 import com.asap.hackathonasap.Repository.PersonRepository;
 import com.asap.hackathonasap.Repository.TransactionRepository;
+import com.asap.hackathonasap.model.Person;
+import com.asap.hackathonasap.model.Transaction;
 
 @SpringBootApplication
 public class HackathonasapApplication implements CommandLineRunner {
@@ -30,13 +32,30 @@ public class HackathonasapApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
-		
+		try {
+			processInputData("input-data.csv");
+			processConciliationData("conciliation-data.csv");
+		}catch (IOException e) {
+			e.printStackTrace();
+		}		
 	}
 	
 	private void processInputData(String filePath) throws IOException {
+		List<String[]> inputRows = readCSV(filePath);
 		
+		for(String[] row : inputRows) {
+			Person person = createOrUpdatePerson(row);
+			Transaction transaction = createTransaction(row, person.getId());
+			createInstallment(row, transaction.getId());
+		}
+	}
+	
+	private void processConciliationData(String filePath) throws IOException {
+		List<String[]> conciliationRows = readCSV(filePath);
 		
+		for (String[] row : conciliationRows) {
+			reconcileData(row);
+		}
 	}
 
 	
