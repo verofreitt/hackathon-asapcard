cc606d58e5bbb2616d3105069ed473e9ca5cd096
Matheus Libanio <matheus.alexandre.libanio@gmail.com>
2024-02-03 11:55:25 -0300

    leitura do arquivo adicionada

--- src/main/java/com/asap/hackathonasap/HackathonasapApplication.java
+++ src/main/java/com/asap/hackathonasap/HackathonasapApplication.java
@@ -1,5 +1,6 @@
 package com.asap.hackathonasap;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
@@ -13,6 +14,11 @@ import com.asap.hackathonasap.Repository.PersonRepository;
 import com.asap.hackathonasap.Repository.TransactionRepository;
 import com.asap.hackathonasap.model.Person;
 import com.asap.hackathonasap.model.Transaction;
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
 
 @SpringBootApplication
 public class HackathonasapApplication implements CommandLineRunner {
@@ -34,31 +40,30 @@ public class HackathonasapApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		try {
 			processInputData("input-data.csv");
-			processConciliationData("conciliation-data.csv");
 		}catch (IOException e) {
 			e.printStackTrace();
 		}		
 	}
 	
-	private void processInputData(String filePath) throws IOException {
-		List<String[]> inputRows = readCSV(filePath);
+	private void processInputData(String filePath) throws IOException, CsvException {
+        FileReader filereader = new FileReader(filePath); 
 		
-		for(String[] row : inputRows) {
-			Person person = createOrUpdatePerson(row);
-			Transaction transaction = createTransaction(row, person.getId());
-			createInstallment(row, transaction.getId());
-		}
-	}
+        CSVParser parser = new CSVParserBuilder().withSeparator(';').build();
+        
+        CSVReader csvReader = new CSVReaderBuilder(filereader) 
+                .withCSVParser(parser) 
+                .build(); 
+        
+        List<String[]> lerTodasLinhas= csvReader.readAll(); 
+        
+        
+        // Print Data. 
+        for (String[] row : lerTodasLinhas) { 
+            for (String cell : row) { 
+                System.out.print(cell + "\t"); 
+            } 
+            System.out.println(); 
+        } 
+    } 
 	
-	private void processConciliationData(String filePath) throws IOException {
-		List<String[]> conciliationRows = readCSV(filePath);
-		
-		for (String[] row : conciliationRows) {
-			reconcileData(row);
-		}
-	}
-
-	
-	
-
 }
